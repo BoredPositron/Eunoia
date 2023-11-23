da60f22ac954e8cec772fe40bf678441a940a955
attlog <97865180+BoredPositron@users.noreply.github.com>
2023-11-23 21:22:17 +0530

    Added bottom bar on home screen
    Functionality for bottom bar is still missing.

--- app/src/main/java/com/eunoia/app/user/user_interface/HomeScreen.kt
+++ app/src/main/java/com/eunoia/app/user/user_interface/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.eunoia.app.user.user_interface
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import com.eunoia.app.R
@@ -11,14 +14,20 @@ import com.eunoia.app.authentication.user_interface.AuthViewModel
 import com.eunoia.app.navigation.ROUTE_HOME
 import com.eunoia.app.navigation.ROUTE_LOGIN
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: AuthViewModel?, navController: NavHostController?){
-    Column {
+    Scaffold(
+        bottomBar = {
+            BottomBar(viewModel = viewModel, navHostController = navController)
+        }
+    ) {
         Button(
+            modifier = Modifier.padding(it),
             onClick = {
                 viewModel?.logout()
-                navController!!.navigate(ROUTE_LOGIN){
-                    popUpTo(ROUTE_HOME){inclusive = true}
+                navController!!.navigate(ROUTE_LOGIN) {
+                    popUpTo(ROUTE_HOME) { inclusive = true }
                 }
             }
         ) {
