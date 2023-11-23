90dc05811f9081a2e96daa2d08dff0528228237f
attlog <97865180+BoredPositron@users.noreply.github.com>
2023-11-23 21:10:22 +0530

    Sign out button implmented on home screen

--- app/src/main/java/com/eunoia/app/user/user_interface/HomeScreen.kt
+++ app/src/main/java/com/eunoia/app/user/user_interface/HomeScreen.kt
@@ -1,10 +1,28 @@
 package com.eunoia.app.user.user_interface
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.eunoia.app.R
 import com.eunoia.app.authentication.user_interface.AuthViewModel
+import com.eunoia.app.navigation.ROUTE_HOME
+import com.eunoia.app.navigation.ROUTE_LOGIN
 
 @Composable
-fun HomeScreen(viewModel: AuthViewModel, navController: NavHostController){
-
+fun HomeScreen(viewModel: AuthViewModel?, navController: NavHostController?){
+    Column {
+        Button(
+            onClick = {
+                viewModel?.logout()
+                navController!!.navigate(ROUTE_LOGIN){
+                    popUpTo(ROUTE_HOME){inclusive = true}
+                }
+            }
+        ) {
+            Text(text = stringResource(id = R.string.LOGOUT))
+        }
+    }
 }
